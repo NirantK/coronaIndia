08bee0092a26bdcedd0eb364b1751f9be23dbd10
meghanabhange <meghana.bhange@cumminscollege.in>
2020-04-08 17:31:29 +0530

    Add docstrings and comments

--- test_relationship_server.py
+++ test_relationship_server.py
@@ -1,8 +1,12 @@
-from relationship_server import record_processor
 import pytest
+
+from relationship_server import record_processor
 
 
 def test_record_processor():
+    """
+    Testing record_processor: Should return empty list when there is no input.
+    """
     assert record_processor("   ") == {
         "nationality": [],
         "travel": [],
@@ -18,6 +22,8 @@ def test_record_processor():
 
 
 def test_nationality():
+    """Checks Nationality
+    """
     assert record_processor("Indian Student Studying in Italy") == {
         "nationality": ["Indian"],
         "travel": [],
@@ -27,13 +33,20 @@ def test_nationality():
 
 
 def test_travel():
+    """Checks if the Travel place is returned with appropriate `place_attributes`
+    """
     assert record_processor("Traveled from Italy") == {
         "nationality": [],
         "travel": ["Italy"],
         "relationship": [],
         "place_attributes": [{"place": "Italy", "is_foreign": True}],
     }
-
+    assert record_processor("Traveled to Pune") == {
+        "nationality": [],
+        "travel": ["Pune"],
+        "relationship": [],
+        "place_attributes": [{"place": "Pune", "is_foreign": False}],
+    }
     assert record_processor("Traveled from United Kingdom") == {
         "nationality": [],
         "travel": ["United Kingdom"],
@@ -43,6 +56,8 @@ def test_travel():
 
 
 def test_travel_acronymns():
+    """Checks for acronymn to country name mapping
+    """
     assert record_processor("Traveled to UK") == {
         "nationality": [],
         "travel": ["United Kingdom"],
@@ -79,12 +94,14 @@ def test_travel_acronymns():
 
 
 def test_travel_aliases():
+    """Checks for Aliases to country name mapping
+    """
     assert record_processor("Traveled from Republic of South Korea") == {
         "nationality": [],
         "travel": ["South Korea"],
         "relationship": [],
         "place_attributes": [{"place": "South Korea", "is_foreign": True},],
-    }
+    }  # Republic is picked up as a relationship
 
     assert record_processor("Traveled from Aphsny Axwynthkharra") == {
         "nationality": [],
@@ -102,18 +119,22 @@ def test_travel_aliases():
 
 
 def test_relationship():
+    """Relationship mapping
+    """
     assert record_processor("Son of P13") == {
         "nationality": [],
         "travel": [],
         "relationship": [{"link": "Son", "with": ["P13"]}],
         "place_attributes": [],
     }
+    # Same link with Multiple people
     assert record_processor("Son of P13 and P14") == {
         "nationality": [],
         "travel": [],
         "relationship": [{"link": "Son", "with": ["P13", "P14"]}],
         "place_attributes": [],
     }
+    # Multiple Links
     assert record_processor("Son of P13 and P14 friend of P16, P17, P18") == {
         "nationality": [],
         "travel": [],
